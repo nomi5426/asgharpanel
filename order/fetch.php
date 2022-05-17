85d0726ebeac211b42ec3c0a217cfb9f379989e6
nomi5426 <nomankhan729@hotmail.com>
2022-05-17 17:50:43 +0400

    fix qr code

--- order/fetch.php
+++ order/fetch.php
@@ -101,7 +101,7 @@
         //FETCH ITEM
         $oneRow = "$row[6]<br><strong> Size - $row[8]</strong>";
         $deliveryNotePrintqr = "<a href=../base/deliveryNoteDownload.php?file_id=$row[0]>$row[4]</a><br><a href='../qrcodes/$row[22]' target='_blank'><img src='../qrcodes/$row[22]' /></a> ";
-        $qrcodeinvoicefordelivery = "$row[22]";
+        $qrcodeinvoicefordelivery = "$row[0]";
         $deliveryNotePrint = "<a href=../base/deliveryNoteDownload.php?file_id=$row[0]>$row[4] ";
 
         //MATERIAL AVAILABILITY
