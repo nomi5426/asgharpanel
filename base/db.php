f2001f2a960218082e6a1415a4d8d514606f5450
Roshan Zaid <roshan.zaid@live.com>
2022-06-05 11:39:09 +0400

    db_change

--- base/db.php
+++ base/db.php
@@ -29,7 +29,7 @@
 	$host = "localhost";
 	$username = "root";
 	$password = "";
-	$dbname = "creati55_crud";
+	$dbname = "asgharpanel";
 	//MAKES THE CONNECTION
 	$conn = mysqli_connect($host, $username, $password, $dbname);
 	if (!$conn) {
