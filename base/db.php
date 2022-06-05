3d807e645e2d6d777931b8b631b7407bf4b784cf
Roshan Zaid <roshan.zaid@live.com>
2022-06-05 11:46:35 +0400

    db_changes

--- base/db.php
+++ base/db.php
@@ -1,40 +1,13 @@
 <?php
 
-	/*********************************************************************************
-	* PROJECT: ZETA 1.0.0
-	* AUTHOR: ROSHAN ZAID AKA DAUNTE
-	* FILE FOR: DATABASE MANAGEMENT
-	* 
-	* VARIABLES
-	* @PARAM	{STRING}	HOST								//DB HOST
-	* @PARAM	{STRING}	USERNAME							//HOST USERNAME
-	* @PARAM	{STRING}	PASSWORD							//HOST PASSWORD
-	* @PARAM	{STRING}	DBNAME								//DATABASE NAME
-	* @PARAM	{STRING}	CONN								//DB CONNECT VARIABLE
-	* @PARAM	{STRING}	MESSAGE								//LOG MESSAGE
-	* @PARAM	{STRING}	LOGFILE								//LOG FILE PATH
-	*
-	/********************************************************************************/
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "asgharpanel";
 
-	/**
-	 * MASTER METHOD FOR LOG TRACKING
-	 * @PARAM {STRING}	MESSAGE
-	 */
-	// function app_log($message){
-	// 	date_default_timezone_set('Asia/Dubai');
-	// 	$logfile = 'log/log_'.date('d-M-Y').'.log';
-	// 	file_put_contents($logfile, $message . "\n", FILE_APPEND);
-	// }
-
-	$host = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "asgharpanel";
-	//MAKES THE CONNECTION
-	$conn = mysqli_connect($host, $username, $password, $dbname);
-	if (!$conn) {
-		// app_log("'".date('d-m-Y H:i:s')."' : DATABASE connection hasn't been made. Please check DB Config;");
-		die("Connection failed: " . mysqli_connect_error());
-	}
+$conn = mysqli_connect($host, $username, $password, $dbname);
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
 
 ?>
