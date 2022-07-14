0f34d25a77ed0a16898e4a4bada552efc65977f6
Roshan Zaid <roshan.zaid@live.com>
2022-07-14 11:48:12 +0400

    delivery staff mark

--- base/delivery.php
+++ base/delivery.php
@@ -66,6 +66,7 @@ if(!isset($_SESSION['_deliveryLogin'])){header('Location:../index.php');}
                                                           <th class="border-bottom-0">Consult</th>
                                                           <th class="border-bottom-0">Image</th>
                                                           <th class="border-bottom-0">Comment</th>
+                                                          <th class="border-bottom-0">Action</th>
                                                         </tr>
                                                       </thead>
                                                     </table>
@@ -103,7 +104,86 @@ if(!isset($_SESSION['_deliveryLogin'])){header('Location:../index.php');}
                 <!-- Back-to-top -->
                 <a href="#top" id="back-to-top"><i class="las la-angle-double-up"></i></a>
 
-                <script>
+
+                <script type="text/javascript">
+                    $(document).ready(function() {
+                        var table = $('#deliverytable').DataTable( {
+                            "processing": 	true,
+                            "serverSide": 	true,
+                            "paging"	:	true,
+                            "searching"	:	true,
+                            "sDom": 'Brtip',
+                            "buttons": [
+                                
+                            ],
+                            "iDisplayLength"	:	100,
+                            "ajax": {
+                                url  :"../order/fetch.php",
+                                type : "POST",
+                                data : {
+                                    status : 'Out for Delivery'
+                                }
+                            },
+                            "rowCallback": function( row, data, index ) {
+                                if ( data[7] == "Sharaf DG" )
+                                {
+                                    $('td', row).css('background-color', '#b5b5de');
+                                }
+                                else if ( data[7] != "Sharaf DG" )
+                                {
+                                    $('td', row).css('background-color', 'white');
+                                }
+                            },
+                            "drawCallback": function ( settings ) {
+                                var api = this.api();
+                                var rows = api.rows( {page:'current'} ).nodes();
+                                var last=null; 
+                                api.column(1, {page:'current'} ).data().each( function ( group, i ) {
+                                    if ( last !== group ) {
+                                        $(rows).eq( i ).before(
+                                            '<tr class="group"><td class="delback"colspan="13">'+'<strong> Delivery On : '+group+'</strong></td></tr>'
+                                        );
+                                        last = group;
+                                    }
+                                } );
+                            },
+                            "autoWidth": false,
+                            "aoColumnDefs": [{ "bSortable": false, "bSearchable": false, "aTargets": [2,4,5,6,7,8,9,10,11,12 ] } ],
+                            "aoColumns": [{ "sWidth": "5%" }, { "sWidth": "5%" },{ "sWidth": "2%" }, { "sWidth": "3%" },{ "sWidth": "2%" },{ "sWidth": "20%" },{ "sWidth": "12%" },{ "sWidth": "3%" },{ "sWidth": "15%" },{ "sWidth": "5%" },{ "sWidth": "3%" },{ "sWidth": "15%" },{ "sWidth": "3%" }]
+                        } );
+                        
+                        $(document).on('click','#statusChangeNext',function(event){
+                            event.preventDefault();
+                            var statusid = $(this).attr('data-id');
+                            var prodStat;
+                            $.ajax({
+                                url     : '../order/statusChange.php',
+                                method  : 'POST',
+                                dataType: 'json',
+                                data    : {statusid : statusid},
+                                success : function(response)
+                                {
+                                    if(response.index == 1){
+                                        swal({
+                                            title: 'Status Changed',
+                                            text: 'Order Status is Changed Succesfully',
+                                            type: 'success',
+                                            confirmButtonColor: '#57a94f',
+                                            allowOutsideClick: true
+                                        });
+                                            $('#deliverytable').DataTable().ajax.reload();
+                                    }
+                                    else{
+                                        console.log('AUL AUL AUL');
+                                    }
+                                }
+                            });
+                        });
+                    });
+                </script>
+
+                <!--NO MAN CODES-->
+                <!-- <script>
 
         function getQueryParams(qs) {
 	        qs = qs.split('+').join(' ');
@@ -178,17 +258,14 @@ if ( query ) {
 					}
 					});
 
-                </script>
+                </script> -->
 
                 <!-- Back-to-top -->
                 <a href="#top" id="back-to-top"><i class="ti-angle-double-up"></i></a>
-
                 <!-- JQuery min js -->
                 <script src="../assets/plugins/jquery/jquery.min.js"></script>
-
                 <!-- Bootstrap Bundle js -->
                 <script src="../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-
                 <!-- Internal Data tables -->
                 <script src="../assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
                 <script src="../assets/plugins/datatable/js/dataTables.dataTables.min.js"></script>
@@ -206,31 +283,25 @@ if ( query ) {
                 <script src="../assets/plugins/datatable/js/buttons.colVis.min.js"></script>
                 <script src="../assets/plugins/datatable/js/dataTables.responsive.min.js"></script>
                 <script src="../assets/plugins/datatable/js/responsive.bootstrap4.min.js"></script>
-
                 <!--Internal  Datatable js -->
                 <script src="../assets/js/table-data.js"></script>
-
                 <!-- eva-icons js -->
                 <script src="../assets/js/eva-icons.min.js"></script>
-
                 <!-- Horizontalmenu js-->
                 <script src="../assets/plugins/horizontal-menu/horizontal-menu-2/horizontal-menu.js"></script>
-
                 <!-- Sticky js -->
                 <script src="../assets/js/sticky.js"></script>
-
                 <!-- Internal Select2 js-->
                 <script src="../assets/plugins/select2/js/select2.min.js"></script>
-
                 <!--Internal Sumoselect js-->
                 <script src="../assets/plugins/sumoselect/jquery.sumoselect.js"></script>
-
                 <script src="../assets/plugins/rating/jquery.rating-stars.js"></script>
                 <script src="../assets/plugins/rating/jquery.barrating.js"></script>
-
+                <!-- Sweet-alert js  -->
+		<script src="../assets/plugins/sweet-alert/sweetalert.min.js"></script>
+		<script src="../assets/js/sweet-alert.js"></script>
                 <!-- custom js -->
                 <script src="../assets/js/custom.js"></script>
-
                 <!-- Internal Modal js-->
                 <script src="../assets/js/modal.js"></script>
 
--- order/fetch.php
+++ order/fetch.php
@@ -308,6 +308,7 @@
             $subdata[]=$row[13];
             $subdata[]='<img src="'.$upload_dir.$image.'" class="modal-effect" data-effect="effect-scale" id="tableImage" height="30" width="20" data-toggle="modal" data-target="#imagemodalone" data-id="'.$row[0].'"/>';
             $subdata[]=$comment;
+            $subdata[]='<div class="inner"><button id="statusChangeNext" title="Next" class="btn btn-primary btn-icon" data-id="'.$_id.'"><i class="typcn typcn-arrow-right"></i></button></div>';
         }
 
         $data[]=$subdata;
